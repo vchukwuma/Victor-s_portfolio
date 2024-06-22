885d323aea82b41370d4626157c1da1db73bf011
vchukwuma <89330614+vchukwuma@users.noreply.github.com>
2024-06-21 20:52:00 -0500

    Update SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -1,1 +1,21 @@
+--- time_of_day
+
+ALTER TABLE sales ADD time_of_day VARCHAR(20);
+
+UPDATE sales
+SET time_of_day = 
+    CASE 
+        WHEN time BETWEEN '00:00:00' AND '11:59:59' THEN 'Morning'
+        WHEN time BETWEEN '12:00:00' AND '15:59:59' THEN 'Afternoon'
+        ELSE 'Evening'
+    END;
+
+
+
+--- day_name
+ALTER TABLE sales ADD day_name VARCHAR(10);
+
+UPDATE sales
+SET day_name = DATENAME(WEEKDAY, date);
+
 
