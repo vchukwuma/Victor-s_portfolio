7486e85fd1a87446817144a1a92f628ba9088b5e
vchukwuma <89330614+vchukwuma@users.noreply.github.com>
2024-06-21 21:03:07 -0500

    Update SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -28,21 +28,88 @@ SET month_name = DATENAME(MONTH, date);
 
 --------------------------- Questions Answered ----------------------------
 
-1. How many unique cities does the data have?
+--- How many unique cities does the data have?
 
 SELECT DISTINCT city FROM sales;
 
-2. In which city is each branch?
+
+--- In which city is each branch?
 
 SELECT DISTINCT branch, city FROM sales;
 
-3. How many unique product lines does the data have?
+
+--- How many unique product lines does the data have?
 
 SELECT COUNT(DISTINCT product_line) AS unique_product_lines FROM sales;
 
 
+--- What is the most selling product line (by quantity)?
+
+SELECT product_line, SUM(quantity) AS qty
+FROM sales
+GROUP BY product_line
+ORDER BY qty DESC;
 
 
+--- What is the most selling product line based on the number of transactions?
+
+SELECT product_line, COUNT(product_line) AS cnt
+FROM sales
+GROUP BY product_line
+ORDER BY cnt DESC;
 
 
+--- What is the total revenue by month?
 
+SELECT month_name AS month, SUM(total) AS total_revenue
+FROM sales
+GROUP BY month_name
+ORDER BY total_revenue DESC;
+
+
+--- What product line had the largest revenue?
+
+SELECT product_line, SUM(total) AS largest_revenue
+FROM sales
+GROUP BY product_line
+ORDER BY largest_revenue DESC;
+
+
+--- What month had the largest COGS?
+
+SELECT month_name AS month, SUM(cogs) AS cogs
+FROM sales
+GROUP BY month_name
+ORDER BY cogs DESC;
+
+
+--- What is the city with the largest revenue?
+
+SELECT branch, city, SUM(total) AS total_revenue
+FROM sales
+GROUP BY city, branch
+ORDER BY total_revenue DESC;
+
+
+--- What is the average rating of each product line?
+
+SELECT product_line, ROUND(AVG(rating), 2) AS avg_rating
+FROM sales
+GROUP BY product_line
+ORDER BY avg_rating DESC;
+
+
+--- Number of sales made in each time of the day per weekday?
+
+SELECT day_name, time_of_day, COUNT(*) AS total_sales
+FROM sales
+GROUP BY day_name, time_of_day
+ORDER BY day_name, total_sales DESC;
+
+
+--- Which customer types bring the most revenue?
+
+SELECT customer_type, SUM(total) AS total_revenue
+FROM sales
+GROUP BY customer_type
+ORDER BY total_revenue DESC;
